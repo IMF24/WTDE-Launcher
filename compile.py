9e55eeb3415f114cccffb89c75e51192b6c337fc
IMF24 <caseylcox25@gmail.com>
2023-07-08 12:46:40 -0400

    Added batch script builder for creating EXE files

---
+++ compile.py
@@ -0,0 +1,23 @@
+from launcher_constants import VERSION
+from cmd_colors import *
+import os as OS
+import sys as SYS
+import time as TIME
+
+arguments = SYS.argv
+
+print(f"{BLUE}-------------------------------")
+print(f"{LIGHT_BLUE}Please wait, compiling build...")
+print(f"{BLUE}-------------------------------{WHITE}")
+
+OS.system(f"python -m PyInstaller -F --clean -y -n \"GHWT Definitive Launcher++ (V{VERSION})\" --add-data=\"res;res\" --windowed --icon=\"res/icon.ico\" main.py")
+
+print(f"{GREEN}Done compiling the EXE!\n{YELLOW}Cleaning up files...")
+
+specFile = f"GHWT Definitive Launcher++ (V{VERSION}).spec"
+
+if (OS.path.exists(specFile)): OS.remove(specFile)
+
+print(f"{GREEN}At long last, all done!")
+
+input(f"{LIGHT_BLUE}? {WHITE}Press ENTER to exit the EXE builder. ")
