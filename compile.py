1f541e86b4a8e1563af7ad04549c76bbb48dba8f
IMF24 <caseylcox25@gmail.com>
2023-07-11 20:42:43 -0400

    More compiler improvements, allows ghde_content sync

--- compile.py
+++ compile.py
@@ -2,20 +2,60 @@ from launcher_constants import VERSION
 from cmd_colors import *
 import os as OS
 import sys as SYS
+import shutil as SHUT
+import configparser as CF
+
+config = CF.ConfigParser(strict = False, allow_no_value = True)
 
 arguments = SYS.argv
+
+if ("-R") in (arguments) or ("--repo-build") in (arguments):
+    dashNName = f"GHWT_Definitive_Launcher"
+else:
+    dashNName = f"GHWT Definitive Launcher++ (V{VERSION})"
+
+if ("-S") in (arguments) or ("--sync") in (arguments):
+    dashNName = f"GHWT_Definitive_Launcher"
+    sendToDevRepoFolder = True
+else:
+    sendToDevRepoFolder = False
 
 print(f"{BLUE}-------------------------------")
 print(f"{LIGHT_BLUE}Please wait, compiling build...")
 print(f"{BLUE}-------------------------------{WHITE}")
 
-OS.system(f"python -m PyInstaller -F --clean -y -n \"GHWT Definitive Launcher++ (V{VERSION})\" --add-data=\"res;res\" --windowed --icon=\"res/icon.ico\" main.py")
+if ("-R") in (arguments) or ("--repo-build") in (arguments): print(f"{YELLOW}Compiling build for developers' repository...{WHITE}")
+if ("-S") in (arguments) or ("--sync") in (arguments): print(f"{YELLOW}Will sync build to developers' repo!{WHITE}")
+
+OS.system(f"python -m PyInstaller -F --clean -y -n \"{dashNName}\" --add-data=\"res;res\" --windowed --icon=\"res/icon.ico\" main.py")
 
 print(f"{GREEN}Done compiling the EXE!\n{YELLOW}Cleaning up files...")
 
-specFile = f"GHWT Definitive Launcher++ (V{VERSION}).spec"
+specFile = f"{dashNName}.spec"
 
 if (OS.path.exists(specFile)): OS.remove(specFile)
+
+if (sendToDevRepoFolder):
+    print(f"{YELLOW}Syncing EXE to ghde_content...{WHITE}")
+
+    if (not OS.path.exists('config.ini')):
+        print(f"{RED}Error: No config.ini file existed, cannot sync!{WHITE}")
+
+    else:
+        config.read('config.ini')
+
+        try:
+            devRepoDir = config.get('DevConfig', 'DevRepo')
+
+            if (OS.path.exists(f"{devRepoDir}\\Packages\\ghde_content\\Content\\GHWT_Definitive_Launcher.exe")):
+                OS.remove(f"{devRepoDir}\\Packages\\ghde_content\\Content\\GHWT_Definitive_Launcher.exe")
+
+            SHUT.copy("dist\\GHWT_Definitive_Launcher.exe", f"{devRepoDir}\\Packages\\ghde_content\\Content\\GHWT_Definitive_Launcher.exe")
+
+            print(f"{GREEN}Compiled EXE synced to dev repo!{WHITE}")
+
+        except Exception as exc:
+            print(f"{RED}Error: Cannot sync to dev repo! Sync failed due to the following error: {exc}.{WHITE}")
 
 print(f"{GREEN}At long last, all done!")
 
